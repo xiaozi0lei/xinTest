873c58f233a83ca8e8c2931736af4c1a8f3e97d7
xiaozi0lei <xiaozi0lei@163.com>
2014-08-19 07:04:34 +0800

    update the port

--- features/step_definitions/http.rb
+++ features/step_definitions/http.rb
@@ -1,11 +1,12 @@
 require 'pry'
 
 假如(/^请求一个(.*)$/) do |url|
-  visit(url)
+#  visit(url)
+  @response = MyHttp.get(url)
 end
 
 那么 /^返回数据应该包含:$/ do |expected_table|
   expected_table.raw.each do |row|
-    expect(page).to have_content(row[0])
+    expect(@response.body).to have_content(row[0])
   end
 end
