915a09f5bda729ba96dcf8dcb98a412c3ddde941
xiaozi0lei <xiaozi0lei@163.com>
2014-09-12 19:45:20 +0800

    comments in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
+ 
+  # 此处为切换中文的方法, 设置I18n的locale为session中带上来的locale值, zh-CN, 对应与根目录下的locale目录下的语言 
   def set_locale
     I18n.locale = session[:locale] || I18n.default_locale
   end
