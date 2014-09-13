f87d1ebc67453ce19148cc3cb28c9d7d7d09c7f4
lixinxin <lixinxin@duoku.com>
2014-09-13 14:29:00 +0800

    update gitignore and add comments for app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,4 @@ class ApplicationController < ActionController::Base
     I18n.locale = session[:locale] || I18n.default_locale
   end
   
-#  def default_url_options(options={})
-#    logger.debug "default_url_options is passed options: #{options.inspect}\n"
-#    { locale: I18n.locale }
-#  end
 end
