f4b22845f75432a9e4eac0a7747ab712c666e5c5
xiaozi0lei <plmmlixinxin@163.com>
2014-12-24 11:26:32 +0800

    解决get用例show页面由于库里缺少参数project而导致异常问题

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140908141056) do
+ActiveRecord::Schema.define(version: 20141224031851) do
 
   create_table "gets", force: true do |t|
     t.text     "url"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140908141056) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "title"
+    t.string   "project"
   end
 
   create_table "posts", force: true do |t|
