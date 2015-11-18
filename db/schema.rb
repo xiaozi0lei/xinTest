568907a0d30608a7741a2fd589831efbef0ef502
sunguolei <sunguolei@baidu-mgame.com>
2015-11-18 11:47:22 +0800

    db schema update

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20150105064113) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "gets", force: true do |t|
     t.text     "url"
     t.string   "result"
