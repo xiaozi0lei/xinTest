1ca790ae17e84d40e13343d722dd92b1649dc64c
sunguolei <sunguolei@baidu-mgame.com>
2015-12-08 14:20:45 +0800

    bugfix and format log style

--- config/environment.rb
+++ config/environment.rb
@@ -7,3 +7,9 @@ Rails.application.initialize!
 WillPaginate::ViewHelpers.pagination_options[:previous_label ]= "前一页"
 WillPaginate::ViewHelpers.pagination_options[:next_label ]= "后一页"
 
+# custom logger format with timestamp
+class Logger  
+  def format_message(level, time, progname, msg)  
+    "#{time.to_s(:db)} #{level} -- #{msg}\n"  
+  end  
+end
